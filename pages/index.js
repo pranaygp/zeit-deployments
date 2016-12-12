25f7ab4c50c7c7d6aa7fb8fe27a885a35d58cc22
Pranay Prakash <pranay.gp@gmail.com>
2016-12-11 20:19:52 -0600

    Add PWA stuff

--- pages/index.js
+++ pages/index.js
@@ -28,6 +28,24 @@ class Index extends Component {
       <div className={css(styles.container)}>
         <Head>
           <meta name='viewport' content='width=device-width, initial-scale=1'></meta>
+          <meta name="og:image" content="https://cdn.zeit.co/zeit/twitter-card.png"/>
+          <link rel="apple-touch-icon" sizes="57x57" href="https://cdn.zeit.co/favicon/apple-touch-icon-57x57.png"/>
+          <link rel="apple-touch-icon" sizes="60x60" href="https://cdn.zeit.co/favicon/apple-touch-icon-60x60.png"/>
+          <link rel="apple-touch-icon" sizes="72x72" href="https://cdn.zeit.co/favicon/apple-touch-icon-72x72.png"/>
+          <link rel="apple-touch-icon" sizes="76x76" href="https://cdn.zeit.co/favicon/apple-touch-icon-76x76.png"/>
+          <link rel="apple-touch-icon" sizes="114x114" href="https://cdn.zeit.co/favicon/apple-touch-icon-114x114.png"/>
+          <link rel="apple-touch-icon" sizes="120x120" href="https://cdn.zeit.co/favicon/apple-touch-icon-120x120.png"/>
+          <link rel="apple-touch-icon" sizes="144x144" href="https://cdn.zeit.co/favicon/apple-touch-icon-144x144.png"/>
+          <link rel="apple-touch-icon" sizes="152x152" href="https://cdn.zeit.co/favicon/apple-touch-icon-152x152.png"/>
+          <link rel="apple-touch-icon" sizes="180x180" href="https://cdn.zeit.co/favicon/apple-touch-icon-180x180.png"/>
+          <link rel="icon" type="image/png" href="https://cdn.zeit.co/favicon/favicon-32x32.png" sizes="32x32"/>
+          <link rel="icon" type="image/png" href="https://cdn.zeit.co/favicon/android-chrome-192x192.png" sizes="192x192"/>
+          <link rel="icon" type="image/png" href="https://cdn.zeit.co/favicon/favicon-96x96.png" sizes="96x96"/>
+          <link rel="icon" type="image/png" href="https://cdn.zeit.co/favicon/favicon-16x16.png" sizes="16x16"/>
+          <link rel="manifest" href="https://cdn.zeit.co/favicon/manifest.json"/>
+          <link rel="mask-icon" href="https://cdn.zeit.co/favicon/safari-pinned-tab.svg" color="#ffffff"/>
+          <link rel="shortcut icon" href="https://cdn.zeit.co/favicon/favicon.ico"/>
+          <meta name="theme-color" content="#000"/>
         </Head>
         <h1 className={css(styles.title)}>My ▲ZEIT Deployments</h1>
         <ol className={css(styles.list)}>
